46fd2fc5baee1de510c8e284db9c896ecc031d81
rob <schlenk7@gmail.com>
2020-04-28 08:39:50 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,76 @@
+let name = "Rob";
+let age = 33;
+let birthday = "June 23";
+let detroitGC = true;
+let lifeEvents = ["I went to MSU", "I have a baby boy named Elliott", "My most recent vacation was in key West", "I like to cook and read"];
+
+
+if (detroitGC === true) {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Detroit Michigan`);
+}
+else {
+    console.log(`My name is ${name} and I am a student at Grand Circus in Grand Rapids`);
+}
+
+for (let i = 0; i < lifeEvents.length; i++) {
+    console.log(lifeEvents[i]);
+}
+
+
+
+var counter = 0;
+
+
+while (true) {
+    var randomNumber = Math.floor(Math.random() * 10) + 1;
+    if (randomNumber !== 5) {
+        counter++;
+        console.log(`${randomNumber} !==5 `);
+    }
+    else {
+        counter++
+        console.log(`5 === 5. It took ${counter}
+    iterations to randomly generate the number 5.`); { break };
+    };
+}
+
+
+
+
+
+
+
+
+
+
+// for (let i = 0; i < favoriteMusic.length; i++) {
+//     console.log(favoriteMusic[i]);
+// }
+// if (randomNumber !== 5; i !== 5; counter++) {
+//     console.log("randomNumber !==5");
+// }
+// else (counter++) {
+//     console.log()
+// }
+
+// let counter = 0;
+// let randomNumber = Math.floor(Math.random() * 10) + 1;
+// while(randomNumber >= 1 && randomNumber <= 10)
+
+
+
+
+
+// // if (randomNumber !== 5) {
+// //     console.log("randomNumber !==5")
+// // } else (randomNumber i++
+
+
+// while (true) {
+//     if (randomNumber !== 5)
+//     ===
+// }
+
+
+
+
